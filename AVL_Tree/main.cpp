56638b47791d86922fbfb09a3b644bfcbd6d2305
levon <levon>
2024-01-29 17:38:07 +0400

    Avl_changes

--- AVL_Tree/main.cpp
+++ AVL_Tree/main.cpp
@@ -1,39 +1,23 @@
 #include "AVL_tree.hpp"
 #include <iostream>
 
-template <typename T>
-void do_print(Node<T>* root, int level) {
-  if (root == nullptr) {
-    return;
-  }
-    const int dist = 4;
-
-    do_print<T>(root->left, level + 1);
-
-    for(int i = 0; i < level * dist; ++i) {
-      std::cout << " ";
-    }
-    std::cout << root->value << '\n';
-
-    do_print<T>(root->right, level + 1);
-}
-
 int main() {
   
-  Node<int>* root = nullptr;
-  root = insert(root, 12);
-  root = insert(root, 8);
-  root = insert(root, 4);
-  root = insert(root, 16);
-  root = insert(root, 10);
-  root = insert(root, 18);
-  root = insert(root, 11);
-  root = insert(root, 21);
-  root = insert(root, 7);
+  AVL<int>  root;
+  root.insert(20);
+  root.insert(25);
+  root.insert(9);
+  root.insert(12);
+  root.insert(27);
+  root.insert(6);
+  root.insert(14);
+  root.insert(4);
+  root.insert(10);
+  // root.delete_node(10);
 
-  root = delete_node(root, 10);
-
-  do_print<int>(root, 0);
+  //root.print();
+  std::cout << std::endl;
+  // root.get_successor(14);
 
   return 0;
 }
